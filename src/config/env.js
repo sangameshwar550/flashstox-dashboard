cee559de11fc8a1de5609916946c6ba04df31940
dev sanga <sangameshwar550@gmail.com>
2026-04-27 00:39:15 +0530

    added hostname

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ const ENV = {
     BASE_URL: "http://localhost:8081",
   },
   prod: {
-    BASE_URL: "https://your-cloud-run-url.a.run.app",
+    BASE_URL: "https://flashstox-node-app-git-869534904643.asia-south1.run.app",
   },
 };
 
